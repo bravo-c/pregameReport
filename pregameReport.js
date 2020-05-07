bd49913b39c2258d9a46e769f237f9ff19372b93
Christopher Bravo <bravo.c@husky.neu.edu>
2020-05-07 15:29:40 -0400

    a little progress adding a lot more functions

--- pregameReport.js
+++ pregameReport.js
@@ -1,5 +1,7 @@
 const _ = require('lodash');
 const champions = require('./resources/champions.json');
+const items = require('./resources/item.json');
+const ornnUpgradesData = require('./resources/ornnUpgrades.json');
 
 // getChampData: String -> ChampionObject (has a lot of fields)
 // should be called in try catch loop if you don't want program to crash
@@ -70,18 +72,146 @@ function prettyPrintArmor(champName) {
     }
 }
 
-// prettyPrintHP('Jax');
-// prettyPrintHP('Zed');
-prettyPrintArmor('Zed');
+function listPrinter(objData, sortFunc, mapFunc) {
+    let genericList = [];
+    _.forEach(objData, (obj) => {
+        genericList.push(obj);
+    });
+    genericList = genericList.sort(sortFunc).map(mapFunc);
+    _.forEach(genericList, (element) => {
+        console.log(element);
+    });
+}
 
-let cList = [];
-_.forEach(champions.data, (champ) => {
-    cList.push(champ);
-});
-cList = cList.sort((x, y) => {
-    return x.stats.armor - y.stats.armor;
-})
-let sortedList = cList.map((x) => `${x.name}:${x.stats.armor}`);
-_.forEach(sortedList, (element) => {
-    console.log(element);
-});
+// print all champs sorted by armor
+listPrinter(
+    champions.data,
+    (x, y) => {
+        return x.stats.armor - y.stats.armor;
+    }, 
+    (x) => `${x.name} :: ${x.stats.armor}`);
+
+// let cList = [];
+// _.forEach(champions.data, (champ) => {
+//     cList.push(champ);
+// });
+// cList = cList.sort((x, y) => {
+//     return x.stats.armor - y.stats.armor;
+// })
+// let sortedArmorList = cList.map((x) => `${x.name} :: ${x.stats.armor}`);
+// _.forEach(sortedArmorList, (element) => {
+//     console.log(element);
+// });
+
+// let itemsList = [];
+// _.forEach(items.data, (item) => {
+//     itemsList.push(item);
+// });
+// itemsList = itemsList.sort((x, y) => {
+//     return x.gold.total - y.gold.total;
+// });
+// let sortedItemList = itemsList.map((x) => `${x.name} :: ${x.gold.total} total Gold :: ${JSON.stringify(x.stats)}`);
+// _.forEach(sortedItemList, (element) => {
+//     console.log(element);
+// });
+
+// console.log();
+// console.log(`total num champs: ${cList.length}`);
+// console.log(`total num items: ${itemsList.length}`);
+
+// function getItemById(itemId) {
+//     return items.data[itemId];
+// }
+
+// function getItemByName(itemName) {
+//     let found;
+//     _.forEach(items.data, (item) => {
+//         if (item.name.toLowerCase() === itemName.toLowerCase()) {
+//             found = item;
+//         }
+//     });
+//     return found || 'MISSION FAILED WE\'LL GET THEM NEXT TIME LADS.';
+// }
+
+// console.log(getItemById(1001));
+// console.log(getItemByName('boots of speed'));
+// console.log(getItemByName('rabadon\'s deathcap'));
+// console.log(getItemById(3374));
+
+// function getChampByName(champName) {
+//     let found;
+//     _.forEach(champions.data, (champ) => {
+//         if (champ.name.toLowerCase() === champName.toLowerCase()) {
+//             found = champ;
+//         }
+//     });
+//     return found || 'damn';
+// }
+
+// let blocks = ornnUpgradesData.blocks;
+// let blockList = [];
+// _.forEach(blocks, (block) => {
+//     if (block.type.toLowerCase() === 'ornnupgrades') {
+//         blockList.push(block);
+//     }
+// });
+// _.forEach(blockList[0].items, (item) => {
+//     console.log(item);
+//     console.log(getItemById(item.id));
+// });
+
+/* * * * * * * * * * * * * * * * * * * * * * * 
+ *   THIS SECTION CALCULATES STAT VALUES     *
+ * * * * * * * * * * * * * * * * * * * * * * */
+
+// let longSwordItem = getItemByName('long sword');
+// let longSwordCost = longSwordItem.gold.total;
+// let statValue = longSwordItem.stats.FlatPhysicalDamageMod;
+// let goldPerAd = longSwordCost / statValue;
+// let adPerGold = statValue / longSwordCost;
+// console.log(longSwordItem);
+// console.log(`${goldPerAd} gold is worth 1 AD.`); 
+// console.log(`1 gold is worth ${adPerGold} AD`); 
+
+// let ampTome = getItemByName('amplifying tome');
+// let ampTomeCost = ampTome.gold.total;
+// let statValue2 = ampTome.stats.FlatMagicDamageMod;
+// let goldPerAp = ampTomeCost / statValue2;
+// let apPerGold = statValue2 / ampTomeCost;
+// console.log(ampTome);
+// console.log(`${goldPerAp} gold is worth 1 AP.`); 
+// console.log(`1 gold is worth ${apPerGold} AP`); 
+
+function mapWeirdStatsToNormalNames(weirdName) {
+    switch(weirdName.toLowerCase) {
+        case 'PercentAttackSpeedMod'.toLowerCase():
+            return 'AS';
+        case 'FlatSpellBlockMod'.toLowerCase():
+            return 'MR';
+        case 'PercentLifeStealMod'.toLowerCase():
+            return 'LIFESTEAL';
+        case 'FlatPhysicalDamageMod'.toLowerCase():
+            return 'AD';
+        case 'FlatMagicDamageMod'.toLowerCase():
+            return 'AP';
+        default:
+            console.error(`${weirdName} doesn't have a normal name.`);
+            throw 'Ah shit, mb.';
+    }
+}
+
+function mapNormalNamesToWeirdStatsNames(normalName) {
+    switch(normalName.toLowerCase) {
+        case 'as':
+            return 'PercentAttackSpeedMod';
+        case 'mr':
+            return 'FlatSpellBlockMod';
+        case 'lifesteal':
+            return 'PercentLifeStealMod';
+        case 'ad':
+            return 'FlatPhysicalDamageMod';
+        default:
+            console.error(`${normalName} doesn't have a weird name.`);
+            throw 'Ah shit, mb.';
+    }
+}
